703c5080a67efcfb5854d7d191387d68b372b90e
yh2237 <2237.yh.2237@gmail.com>
2025-11-07 01:31:10 +0900

    APIドキュメントを更新し、埋め込み機能を実装。

--- frontend/js/jsonget.js
+++ frontend/js/jsonget.js
@@ -15,3 +15,19 @@ fetch("/data/response2.json")
     .catch((err) => {
         document.getElementById("response2").textContent = "読み込み失敗: " + err;
     });
+fetch("/data/response3.json")
+    .then((res) => res.text())
+    .then((text) => {
+        document.getElementById("response3").textContent = text;
+    })
+    .catch((err) => {
+        document.getElementById("response3").textContent = "読み込み失敗: " + err;
+    });
+fetch("/data/response4.json")
+    .then((res) => res.text())
+    .then((text) => {
+        document.getElementById("response4").textContent = text;
+    })
+    .catch((err) => {
+        document.getElementById("response4").textContent = "読み込み失敗: " + err;
+    });
