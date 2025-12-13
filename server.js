099177d921ec52a9261fd144f7adfc34d83c0cfc
yh2237 <2237.yh.2237@gmail.com>
2025-12-14 02:21:49 +0900

    気になってた空白を削除

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.use('/api', apiRoutes);
 // ============================================================
 // ============================================================
 
-app.use((req, res, next) => { 
+app.use((req, res, next) => {
     res.status(404);
     return res.json({ error: `404 Not Found (´・ω・｀). Please see ${config.server.url}/docs for API specifications etc.` });
 });
