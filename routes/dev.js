be3fc298c38b09c65395e738f5857cbc3d2d9688
yh2237 <2237.yh.2237@gmail.com>
2026-01-01 15:39:14 +0900

    /devのroutes

---
+++ routes/dev.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const yaml = require('js-yaml');
+const path = require('path');
+const fs = require('fs');
+
+const configPath = path.join(__dirname, '..', 'config', 'config.yml');
+const config = yaml.load(fs.readFileSync(configPath, 'utf8'));
+
+if (config.server.dev_frontend) {
+  router.use(express.static(path.join(__dirname, '..', 'frontend')));
+
+  router.get('/index', (req, res) => {
+    res.redirect(301, '/');
+  });
+
+  router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
+  });
+
+  router.get('/10cont', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', '10cont.html'));
+  });
+
+  router.get('/search', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'search.html'));
+  });
+
+  router.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'about.html'));
+  });
+
+  router.get('/docs', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'docs.html'));
+  });
+
+  router.get('/db', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'db.html'));
+  });
+
+  console.log("[INFO] 開発用フロントエンド配信: 有効");
+} else {
+  console.log("[INFO] 開発用フロントエンド配信: 無効");
+}
+
+module.exports = router;
